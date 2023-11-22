04981dec248c7a6ade8ee7d29bd8aa9f8bb9aaf6
pcagudelo <17679987+pcagudelo@users.noreply.github.com>
2023-11-21 20:52:19 -0500

    Pantalla de login

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,30 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+<div class="container text-center">
+  <h1>Policasos</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="mb-3">
+    <label for="exampleFormControlInput1" class="form-label">Usuario</label>
+    <input type="text" class="form-control" id="exampleFormControlInput1">
+  </div>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <div class="mb-3">
+    <label for="exampleFormControlInput1" class="form-label">Contraseña</label>
+    <input type="password" class="form-control" id="exampleFormControlInput1">
+  </div>
 
-  <RouterView />
+
+    <button type="button" class="btn btn-success">Ingresar</button>
+
+    
+</div>
+
+
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
